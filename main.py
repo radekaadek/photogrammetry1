5690637b7b5bf033a0ebf6ed12733a1977128244
radekaadek <radekaadek@gmail.com>
2024-05-06 10:19:40 +0200

    old

---
+++ main.py
@@ -0,0 +1,386 @@
+# 1
+# Opracowanie: W. Ostrowski
+# ZFTiSIP GIK PW
+# Projekt nalotu Fotogrametrycznego
+# W CELU WYKONANIA ORTOFOTOMAPY
+# Źródło: OPEGIEKA Elbląg
+# 2
+# Przedmiotem ćwiczenia jest wykonanie części obliczeniowej i graficznej projektu nalotu fotogrametrycznego – misji fotolotniczej. Celem projektowanego nalotu jest wykonanie zdjęć, które posłużą do produkcji cyfrowej ortofotomapy o zadanej wielkości
+# piksela dla obszaru gminy. Takie opracowania przygotowywane są w podziale na arkusze układu 1992 w skali 1:5000.
+# W poniższym ćwiczeniu pomija się kwestię NMT, który jest niezbędny do wykonania
+# ortofotomapy.
+# Podstawowe wzory i nazewnictwo:
+# f – ogniskowa
+# W – wysokość fotografowania
+# GSD – terenowy rozmiar piksela GSD =
+# px
+# W∙f
+# m – skala zdjęcia m =
+# f
+# W
+# lX – rozmiar matrycy wzdłuż kierunku lotu
+# lY – rozmiar matrycy wzdłuż kierunku lotu
+# LX – terenowy zasięg zdjęcia wzdłuż kierunku lotu LX = lX
+# ∙ GSD
+# LY – terenowy zasięg zdjęcia w poprzek kierunku lotu LY = lY
+# ∙ GSD
+# p – pokrycie podłużne
+# q – pokrycie poprzeczne
+# BX – baza podłużna BX = LX
+# ∙
+# 100−p
+# 100
+# BY – baza poprzeczna BY = LY
+# ∙
+# 100−q
+# 100
+# PZ – terenowa powierzchnia zdjęcia PZ = LX
+# ∙ LY
+# PM – terenowa powierzchnia modelu stereoskopowego PM = (LX − BX
+# ) ∙ LY
+# PN – powierzchnia użyteczna ("nowa") modelu PN = BX
+# ∙ BY
+# DX – długość obszaru opracowania wzdłuż kierunku lotu
+# DY – długość obszaru opracowania w poprzek kierunku lotu
+# NY – Liczba szeregów NY =
+# DX
+# BX
+# + 4
+# NX – Liczba zdjęć w szeregu NX =
+# DY
+# BY
+# 3
+# Pierwszym etapem ćwiczenia jest wyznaczanie obszaru opracowania tj. ile arkuszy
+# będzie składało się na opracowanie. Na tym etapie należy podjąć również decyzję o
+# kierunku wykonywania nalotów, które na ogół wykonuje się wzdłuż linii północ-południe lub wschód-zachód.
+# Przyjmuje się, że kierunek nalotu powinien równoległy do dłuższej krawędzi obszaru
+# opracowania, podczas projektowania nalotu należy mieć na uwadze względy ekonomiczne – ważne jest by sumaryczny czas pracy samolotu był jak najkrótszy.
+# Projektując misję fotolotniczą dla konkretnego obszaru należy uwzględnić następujące parametry zdjęć:
+# 1. Projektowany GSD (ang. Ground Sampling Distance);
+# 2. Wysokość fotografowania
+# 3. Pokrycie zdjęć
+# W pierwszej kolejności należy określić minimalną dopuszczalną odległość próbkowania na terenie czyli GSD, innymi słowy będzie to wielkość terenowa odpowiadająca
+# pojedynczemu pikselowi na zdjęciu. Jest to kluczowy parametr charakteryzujący jakość zdjęć cyfrowych. Przy produkcji cyfrowej ortofotomapy zakłada się, że wielkość
+# GSD powinna być mniejsza lub równa wielkości piksela wyjściowej ortofotomapy.
+# Wielkość GSD jest bezpośrednio uzależniona od
+# wysokością lotu (W), relacje pomiędzy tymi dwoma
+# wielkościami opisuje następująca proporcja:
+# px
+# f
+# =
+# GSD
+# W
+# Gdzie: px to wielkość piksela na matrycy kamery,
+# f to ogniskowa kamery,
+# W to wysokość lotu nad poziomem gruntu.
+# Rozmiar piksela na matrycy (px) i długość ogniskowej (f) są stałymi parametrami kamery fotogrametrycznej. Ich wartości (jak również ich wzajemny stosunek) są różne
+# dla poszczególnych modeli cyfrowych kamer fotogrametrycznych. Dlatego też w stosunku do zdjęć cyfrowych nie korzysta się z pojęcia skali zdjęcia (m), zamiast skali
+# jako parametr charakteryzujący zdjęcia podaje się ich GSD.
+# 4
+# Po wyznaczeniu projektowanej wysokości lotu należy skontrolować czy wysokość absolutna lotu (Wabs) nie jest większa od pułapu maksymalnego samolotu:
+# Habs = W + Hśr
+# Gdzie:W to projektowana wysokość lotu,
+# Hśr to średnia wysokość terenu dla obszaru opracowania.
+# Jako średnią wysokość lotu należy przyjąć średnią z wysokości najniżej i najwyżej położonych punktów w obszarze opracowania:
+# Hśr =
+# Hmin + Hmax
+# 2
+# Gdzie: Hmin to minimalna wysokość terenu na obszarze opracowania,
+# Hmax to maksymalna wysokość terenu na obszarze opracowania.
+# Znając wielkość GSD możemy określić zasięg terenowy zdjęcia. Zdjęcia lotnicze wykonane fotogrametrycznymi kamerami cyfrowymi nie zawszę są kwadratowe, tak jak
+# miało to miejsce w przypadku zdjęć analogowych. Kamery zazwyczaj montuje się tak,
+# by dłuższa krawędź zdjęcia była prostopadła do kierunku lotu. Poniżej przedstawiono
+# zasięgi terenowe przykładowych kamer fotogrametrycznych (wraz z GSD) dla wysokości lotu (W) wynoszącej 500 m.
+# 5
+# Dlatego też dla zdjęć cyfrowych wyróżniamy zasięg terenowy wzdłuż i w poprzek kierunku lotu, do ich wyznaczenia wykorzystuje się następujące zależności:
+# LX = lX
+# ⋅ GSD
+# LY = lY
+# ⋅ GSD
+# Gdzie:LX to terenowy zasięg zdjęcia wzdłuż kierunku lotu,
+# LY to terenowy zasięg zdjęcia w poprzek kierunku lotu,
+# lX, lY to odpowiednio liczba pikseli na matrycy kamery
+# wzdłuż i w poprzek kierunku lotu,
+# GSD to wymiar terenowy piksela.
+# Kolejnym etapem jest określenie minimalnej bazy podłużnej (BX) i poprzecznej (BY).
+# Baza podłużna jest odległością pomiędzy kolejnymi miejscami fotografowania (wykonania kolejnych zdjęć) w tym samym szeregu, baza poprzeczna jest natomiast odległością pomiędzy sąsiednimi szeregami. Wielkość bazy jest bezpośrednio powiązana
+# z pokryciem pomiędzy zdjęciami.
+# O pokryciu pomiędzy zdjęciami mówimy wtedy, gdy ten sam obszar odwzorowany
+# jest na dwóch, bądź większej liczbie zdjęć. W klasycznych opracowaniach fotogrametrycznych zakłada się, że każdy fragment opracowywanego obszaru musi zostać odwzorowany przynajmniej na dwóch zdjęciach. Teoretycznie minimalne pokrycie podłużne pozwalające na spełnienie tego warunku wynosi 50% jednakże ze względu na
+# praktyczne czynniki (np. tolerancje nawigacyjne) podczas projektowania typowych
+# misji fotolotniczych przyjmuje się, że pokrycie podłużne (p) nie powinno być mniejsze niż 60%.
+# Pokrycie poprzeczne (pomiędzy szeregami) służy powiązaniu ze sobą kolejnych szeregów, podczas aerotriangulacji w jeden spójny blok, na ogół przyjmuje się, że pokrycie poprzeczne (q) nie powinno być mniejsze niż 30%.
+# 6
+# By wyznaczyć terenowy wymiar baz fotografowania należy skorzystać z poniższych
+# zależności:
+# BX = LX
+# ⋅
+# 100 − p
+# 100
+# BY = LY
+# ⋅
+# 100 − q
+# 100
+# Gdzie:BX to wymiar terenowy bazy podłużnej,
+# BY to wymiar terenowy bazy poprzecznej,
+# LX to terenowy zasięg zdjęcia wzdłuż kierunku lotu,
+# LY to terenowy zasięg zdjęcia w poprzek kierunku lotu,
+# p to pokrycie podłużne,
+# q to pokrycie poprzeczne.
+# Znając wymiar bazy fotografowania oraz obszaru opracowania możemy wyznaczyć
+# liczbę szeregów (NY), która jest wprost zależna od wymiaru bazy poprzecznej (równej
+# odległości pomiędzy szeregami). Drugą z wielkości, którą można określić znając bazę
+# podłużną jest liczba zdjęć w pojedynczym szeregu (NX), która musi uwzględniać to,
+# że pierwsze i ostanie zdjęcie powinny zostać wykonane w odległości 1.5 bazy podłużnej od granicy obszaru opracowania.
+# NY =
+# DY
+# BY
+# NX =
+# DX
+# BX
+# + 4
+# Gdzie:NY to liczba szeregów,
+# NX to liczba zdjęć w pojedynczym szeregu,
+# DY to zasięg obszaru opracowania w poprzek kierunku lotu,
+# BY to wymiar terenowy bazy poprzecznej,
+# DX to zasięg obszaru opracowania wzdłuż kierunku lotu,
+# BX to wymiar terenowy bazy podłużnej.
+# Otrzymane z powyższych wzorów wartości NY i NX na ogół nie będą liczbami całkowitymi, należy je więc zaokrąglić w górę i tak zmodyfikować parametry nalotu by nie
+# zwiększyć obszaru opracowania. Można to uczynić na dwa podstawowe sposoby:
+# − Zmniejszając GSD przy zachowaniu tych samych parametrów pokrycia podłużnego i poprzecznego.
+# − Zwiększając projektowane pokrycie podłużne i poprzeczne, zachowując wielkość GSD.
+# 7
+# Znając ostateczne parametry lotu należy jeszcze skontrolować, czy interwał czasu pomiędzy ekspozycjami (Δt) nie jest mniejszy od cyklu pracy kamery:
+# Δt =
+# BX
+# V
+# Gdzie:Δt to interwał czasu pomiędzy ekspozycjami,
+# BX to wymiar terenowy bazy podłużnej,
+# V to prędkość samolotu.
+# Ostateczną liczbę zdjęć (N) w projekcie, dla prostokątnego bloku obliczamy według
+# wzoru:
+# N = NX
+# ⋅ NY
+# Gdzie:N to liczba zdjęć w bloku,
+# NY to liczba szeregów,
+# NX to liczba zdjęć w szeregu.
+# Jeżeli zaprojektowany blok nie jest prostokątny należy zsumować liczby zdjęć w poszczególnych szeregach.
+# Otrzymaną wielkość należy porównać ze wzorem empirycznym:
+# N = K ∙
+# Pob
+# Pn
+# Gdzie:N to liczba zdjęć w bloku,
+# Pob to powierzchnia obszaru opracowania,
+# Pn to powierzchnia użyteczna modelu,
+# K to współczynnik empiryczny, który w zależności od regularności i wielkości
+# obszaru przyjmuje wartości z zakresu: 1.10 ÷ 1.25.
+# Drugim ważnym parametrem jest czas jaki samolot ma spędzić w powietrzu (powinien być on jak najkrótszy – względy ekonomiczne), podczas jego obliczania należy
+# przyjąć, że pojedynczy nawrót trwa około 140 sekund i uwzględnić czas dolotu do
+# obszaru fotografowania od najbliższego lotniska.
+#  
+# 8
+# Cześć graficzna
+# Cześć graficzna powinna zostać wykonana na mapie topograficznej i zawierać następujące elementy:
+# 1. Granice opracowywanego obiektu (zielony, ciągły gr. 1 mm)
+# 2. Granice ortofotomapy wg. sekcji (niebieski, ciągły gr. 0.2mm)
+# 3. Osie szeregów i punkty nadirowe (czerwony, ciągły, gr. 0.3 mm)
+# 4. Numeracja szeregów (cyfry czerwone wys. 6 mm)
+# 5. Miejsce włączenia i wyłączenia kamery (pierwsze i ostatnie zdjęcie)
+# (niebieski, ciągły, gr. 1 mm, prostopadle do linii nalotu, strzałki na krańcach linii,
+# dł. 1 cm w kierunku lotu)
+# 6. Kierunek północy
+# Cześć graficzna powinna zostać wykonana w formie elektronicznej i przesłana wraz
+# z częścią obliczeniową, na adres mailowy prowadzącego, w postaci pdf.
+# Dane
+# Danymi wejściowymi do ćwiczenia są:
+# 1. Granice zadanej gminy (plik dxf),
+# 2. Podział na arkusze 1:5000 (plik dxf),
+# 3. GSD ortofotomapy,
+# 4. Minimalne pokrycie poprzeczne i podłużne,
+# 5. Dostępne kamery i samoloty.
+# 9
+# Parametry wybranych samolotów fotogrametrycznych:
+# Cessna 402 Cessna T206H NAV III Vulcan Air P68 Obeserver 2 Tencam MMA
+# Źródło: MGGP Aero Źródło: MGGP Aero Źródło: OPEGIEKA Elbląg Źródło: Airborne Technologies
+# Prędkość (km/h): 132-428 Prędkość (km/h): 100-280 Prędkość (km/h): 135-275 Prędkość (km/h): 120-267
+# Pułap (m): 8200 Pułap (m): 4785 Pułap (m): 6100 Pułap (m): 4572
+# Czas lotu (h): 5 Czas lotu (h): 5 Czas lotu (h): 6 Czas lotu (h): 6
+# Parametry wybranych kamer fotogrametrycznych:
+# Z/I DMC IIe 140 Z/I DMC IIe 230 UltraCam Falcon UltraCam Hawk
+# Wymiar matrycy (px): 12096×11200 Wymiar matrycy (px): 15552×14144 Wymiar matrycy (px):14430×9420 Wymiar matrycy (px): 11704×7920
+# Wymiar piksela (μm): 7.2 Wymiar piksela (μm): 5.6 Wymiar piksela (μm): 7.2 Wymiar piksela (μm): 6.0
+# Kanały spektralne: R, G, B, NIR Kanały spektralne: R, G, B, NIR Kanały spektralne:R, G, B, NIR Kanały spektralne: R, G, B, NIR
+# Ogniskowa (mm): 92 Ogniskowa (mm): 92 Ogniskowa (mm): 70 Ogniskowa (mm): 70
+# Cykl pracy (s): 2.2 Cykl pracy (s): 1.8 Cykl pracy (s):1.0 Cykl pracy (s): 2.0
+# Waga (kg): 63 Waga (kg): 63 Waga (kg): 65 Waga (kg): 55
+#  
+# 10
+# Część obliczeniowa - przykładowe opracowanie
+# Przedstawiona powyżej gmina (Pomiechówek), swoim zakresem obejmuje prostokątny obszar o wymiarach 7 (północ-południe) na 9 (wschód-zachód) arkuszy
+# 1:5 000 w układzie 1992. Nalot będzie więc wykonywany ze wschodu na zachód, a
+# wymiary obszaru opracowania wynoszą odpowiednio:
+# DX = 19 090 m
+# DY = 17 219 m
+# Nalot należy zaplanować pod wykonanie ortofotomapy o pikselu równym 25 cm,
+# z wykorzystaniem kamery DMC II 230 i samolotu Tencam MMA.
+# Minimalna wielkość GSD i wysokość lotu wyniosą więc:
+# GSD = 25 cm
+# W = 4107 m
+# Przy przeciętnej wysokości terenu (Hśr) nieprzekraczającej 150 m wysokość absolutna lotu (Wabs) będzie mniejsza niż maksymalny pułap lotu samolotu Tencam
+# MMA, wynoszący 4572 m n.p.m.
+# Dla takiej wysokości lotu uzyskamy następujące zasięgi terenowe zdjęć:
+# LX = 3536 m
+# LY = 3888 m
+# Zakładając minimalne pokrycia p = 60% i q = 30%, bazy wyniosą:
+# BX = 1414 m
+# BY = 2722 m
+# 11
+# Otrzymana liczba szeregów wynosi 6.3, a liczba zdjęć w szeregu 17.5 należy więc
+# tak przeprojektować nalot by składał się on z 7 szeregów po 18 zdjęć.
+# Można to uzyskać zmniejszając bazy do:
+# BX = 1363 m
+# BY = 2460 m
+# Spowoduje to zwiększenie pokryć do:
+# p = 61.5%
+# q = 36.7%
+# Samolot Tencam MMA, pokona odległość dzieląca dwa kolejne zdjęcia w szeregu
+# (BX) w czasie od 18.4 do 40.9 sekund, wynosi więc ona więcej niż cykl pracy kamery (1.8 s).
+# Ze względu na nieregularny kształt obiektu, można spróbować zmniejszyć długość pierwszego (licząc od północy) i trzech ostatnich szeregów:
+# Na nieregularny blok będą się składały kolejno (licząc od północy):
+# jeden szereg z 9 zdjęciami,
+# trzy szeregi po 18 zdjęć,
+# dwa szeregi po 14 zdjęć,
+# jeden szereg z 8 zdjęciami.
+# 12
+# Suma zdjęć wyniesie 96 o następującym rozłożeniu punktów nadirowych:
+# Gdybyśmy pozostali przy bloku o kształcie regularnym to składał by się on z 7
+# szeregów po 18 zdjęć, czyli 126 zdjęć.
+# W sprawozdaniu poza liczbą zdjęć (N) należy jeszcze policzyć i podać:
+# − Sumaryczny czas lotu samolotu.
+# − współczynnik K czyli proporcję pomiędzy powierzchnią nową a powierzchnią opracowania.
+
+import click
+
+# predkosc, pulap, czas
+# matryca px, pixel, kanły spektralne, ogniskowa, cykl pracy, waga
+
+# @click.option('--f', prompt='Podaj ogniskową kamery', help='Ogniskowa kamery', type=float)
+# @click.option('--px', prompt='Podaj px', help='px', type=float)
+# @click.option('--lX', prompt='Podaj lX', help='lX', type=float)
+# @click.option('--lY', prompt='Podaj lY', help='lY', type=float)
+# @click.option('--p', prompt='Podaj p', help='p', type=float)
+# @click.option('--q', prompt='Podaj q', help='q', type=float)
+# @click.option('--DX', prompt='Podaj DX', help='DX', type=float)
+# @click.option('--DY', prompt='Podaj DY', help='DY', type=float)
+# @click.command()
+# @click.option('--W', prompt='Podaj wysokość lotu', help='Wysokość lotu', type=float)
+# def plane(f, w, px, lx, ly, p, q, dx, dy):
+#     if w == 0 or lx == 0 or ly == 0 or f == 0:
+#         print('Wartości w, lx, ly i f nie mogą być równe 0')
+#         return
+#     gsd = px * w / f
+#     m = f / w
+#     lx = lx * gsd
+#     ly = ly * gsd
+#     bx = lx * (100 - p) / 100
+#     by = ly * (100 - q) / 100
+#     pz = lx * ly
+#     pm = (lx - bx) * ly
+#     pn = bx * by
+#     ny = dx / bx + 4
+#     nx = dy / by + 4
+#     ny = round(ny)
+#     nx = round(nx)
+#     print(f'GSD: {gsd}')
+#     print(f'm: {m}')
+#     print(f'LX: {lx}')
+#     print(f'LY: {ly}')
+#     print(f'BX: {bx}')
+#     print(f'BY: {by}')
+#     print(f'PZ: {pz}')
+#     print(f'PM: {pm}')
+#     print(f'PN: {pn}')
+#     print(f'NY: {ny}')
+#     print(f'NX: {nx}')
+
+@click.command()
+# @click.option('--f', prompt='Podaj ogniskową kamery', help='Ogniskowa kamery', type=float)
+# @click.option('-
+# @click.option('--px', prompt='Podaj px', help='px', type=float)
+# @click.option('--lX', prompt='Podaj lX', help='lX', type=float)
+# @click.option('--lY', prompt='Podaj lY', help='lY', type=float)
+def camera_params(f, px, lX, lY):
+    gsd = click.prompt("Podaj GSD", type=float)
+    f = click.prompt("Podaj ogniskową kamery", type=float)
+    px = click.prompt("Podaj px", type=float)
+    lX = click.prompt("Podaj lX", type=float)
+    lY = click.prompt("Podaj lY", type=float)
+    while f == 0 or px == 0 or lX == 0 or lY == 0:
+        click.echo('Wartości f, px, lX i lY nie mogą być równe 0')
+        gsd, m, LX, LY, BX, BY = camera_params()
+    m = f / px
+    LX = lX * gsd
+    LY = lY * gsd
+    BX = LX * (100 - 60) / 100
+    BY = LY * (100 - 30) / 100
+    return gsd, m, LX, LY, BX, BY
+
+
+@click.command()
+def main():
+    click.echo("Podaj Hmin i Hmax")
+    Hmin = click.prompt("Hmin", type=float)
+    Hmax = click.prompt("Hmax", type=float)
+    Hsr = (Hmin + Hmax) / 2
+
+    click.echo("Wybrać domyślną kamerę spośród podanych czy podać parametry kamery?")
+    click.echo("1. Podać parametry kamery")
+    click.echo("2. Z/I DMC IIe 140")
+    click.echo("3. Z/I DMC IIe 230")
+    click.echo("4. UltraCam Falcon")
+    click.echo("5. UltraCam Hawk")
+    choice = click.prompt("Wybierz opcję", type=int)
+    m, LX, LY, BX, BY = 0, 0, 0, 0, 0, 0
+    match choice:
+        case 1:
+            click.echo("Podaj parametry kamery")
+            m, LX, LY, BX, BY = camera_params()
+        case 2:
+            m, LX, LY, BX, BY = camera_params(92, 7.2, 12096, 11200)
+        case 3:
+            m, LX, LY, BX, BY = camera_params(92, 5.6, 15552, 14144)
+        case 4:
+            m, LX, LY, BX, BY = camera_params(70, 7.2, 14430, 9420)
+        case 5:
+            m, LX, LY, BX, BY = camera_params(70, 6.0, 11704, 7920)
+        case _:
+            click.echo("Podano nieprawidłową wartość, podaj parametry kamery")
+            m, LX, LY, BX, BY = camera_params()
+    click.echo(f"Podaj Xmin, Xmax, Ymin, Ymax")
+    Xmin = click.prompt("Xmin", type=float)
+    Xmax = click.prompt("Xmax", type=float)
+    Ymin = click.prompt("Ymin", type=float)
+    Ymax = click.prompt("Ymax", type=float)
+    DX = Xmax - Xmin
+    DY = Ymax - Ymin
+    p = click.prompt("Podaj p", type=float)
+    q = click.prompt("Podaj q", type=float)
+    Wmax = px / (gsd * f)
+    click.echo(f"Wysokość lotu musi być większa niż {Wmax}")
+    click.echo(f"Dostępne samoloty na danym pułapie to:")
+    if Wmax < 4572:
+        click.echo("Tencam MMA")
+    if Wmax < 4785:
+        click.echo("Cessna T206H NAV III")
+    if Wmax < 6100:
+        click.echo("Vulcan Air P68 Obeserver 2")
+    if Wmax < 8200:
+        click.echo("Cessna 402")
+
+
+
+    
+
+
+if __name__ == '__main__':
+    main()
